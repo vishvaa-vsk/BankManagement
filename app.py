4cc615b2cebcdbb50076740db71353f4f159a4fc
vishvaa-vsk <kvishvaa6@gmail.com>
2023-01-23 18:53:45 +0530

    Changes to be committed: modified:   app.py deleted:    chatbotmodel.h5 deleted:    classes.pkl modified:   database.py new file:   ex.py deleted:    intents.json deleted:    requirements.txt new file:   static/history.css modified:   static/home.css modified:   static/reset.css modified:   static/style.css modified:   static/viewacc.css modified:   templates/custdetails.html new file:   templates/history.html modified:   templates/home.html modified:   templates/index.html modified:   templates/navbar.html modified:   templates/resetpasswd.html modified:   templates/signup.html modified:   templates/transfer.html modified:   templates/viewacc.html deleted:    words.pkl

--- app.py
+++ app.py
@@ -1,37 +1,20 @@
-from flask import Flask,render_template,request,redirect, url_for,flash
+from flask import Flask,render_template,request,redirect, url_for,flash,session
 import mysql.connector as sql
-import random,json,pickle
-import numpy as np 
-import os
-import nltk , webbrowser
-from nltk.stem import WordNetLemmatizer
-from tensorflow.keras.models import load_model
-from database import user_passwd
+import random
+from datetime import timedelta ,datetime
 
 
-os.system("pip install -r requirements.txt")
-os.system("python database.py")
-
-mydb = sql.connect(host="localhost",user="root",password=user_passwd,database="bankmanagement",autocommit=True)
+mydb = sql.connect(host="localhost",user="root",password="vishvaa_vsk",autocommit=True,database="bankmanagement")
 cur = mydb.cursor(dictionary=True)
+
 cur.execute("select * from account;")
 usersDic = cur.fetchall()
-status = False
-
-lemmatizer = WordNetLemmatizer()
-intents = json.loads(open("intents.json").read())
-
-words = pickle.load(open('words.pkl','rb'))
-classes = pickle.load(open('classes.pkl','rb'))
-model = load_model("chatbotmodel.h5")
 
 def updateDic():# func which updates dic when new details added!
     global usersDic
     cur.execute("select * from account;")
     usersDic = cur.fetchall()
-def updatestatus():
-    global status
-    status = True
+
 def accnocreate():
     no,pre = random.randint(100000,9999999),"AXB"
     accno = pre+str(no)
@@ -39,122 +22,78 @@ def accnocreate():
         if usersDic[i]["AccNo"]==accno:
             accno = accnocreate()
     return accno
-def status_message():
-    if status == False:
-        flash("Internal server error!")
-        flash("You are logged out due to Safety purposes!")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "8d4a9c0eae5147a030d36c48989892c8"
-
-def clean_up_sentence(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
-    return sentence_words
-
-def bag_of_words(sentence):
-    sentence_words = clean_up_sentence(sentence)
-    bag= [0]*len(words)
-    for w in sentence_words:
-        for i,word in enumerate(words):
-            if word == w:
-                bag[i] = 1
-    return np.array(bag)
-
-def predict_class(sentence):
-    bow = bag_of_words(sentence)
-    res = model.predict(np.array([bow]))[0]
-    ERROR_THRESHOLD = 0.25
-    results = [(i,r) for i,r in enumerate(res) if r > ERROR_THRESHOLD]
-    results.sort(key=lambda x:x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append({'intent': classes[r[0]] , 'probability':str(r[1])})
-    return return_list
-
-def get_response(intents_list , intents_json):
-    tag = intents_list[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if i['tag'] == tag:
-            result = random.choice(i['responses'])
-    if tag== "forget-password":
-        webbrowser.open(request.url_root+"resetpasswd")
-    if tag== "registeration":
-        webbrowser.open(request.url_root+"signup")
-    return result
-
-@app.route("/get")
-def getresponse():
-    userText = request.args.get('msg')
-    ints = predict_class(userText)
-    res = get_response(ints, intents)
-    return str(res)
+app.secret_key = "8d4a9c0eae5147a030d36c48989892c8"
+app.permanent_session_lifetime = timedelta(minutes=3)
 
 @app.route("/",methods=["GET","POST"])# Login route
 def Login():
     if request.method=="POST":
+        session.permanent=True
         accno,passwd=request.form["accno"],request.form["passwd"]
         for i in range(len(usersDic)):
             if usersDic[i]["AccNo"]==accno and usersDic[i]["passwd"]==passwd:
-                updatestatus()
-                return redirect(url_for("home",accno=accno,name=usersDic[i]["Name"]))
-        flash("Invalid credentials! or account not Exist!!")
+                session["Accno"]=accno
+                session["username"] = usersDic[i]["Name"]
+                return redirect(url_for("home"))
+            flash("Invalid credentials! or account not Exist!!")
     return render_template("index.html")
+
+@app.route("/logout")
+def Logout():
+    session.pop('Accno', None)
+    session.pop('username',None)
+    return redirect(url_for('Login'))
 
 @app.route("/signup",methods = ["GET","POST"])
 def Signup():
     updateDic()
     accno = accnocreate()
     if request.method=="POST":#checking POST method!
-        name,address,DOB,cno,obal,email,passwd,repasswd = request.form['name'],request.form['address'],request.form["dob"],int(request.form["cno"]),request.form["obal"],request.form["email"],request.form["passwd"],request.form["re-passwd"]# Gathering all info
+        name,address,DOB,cno,obal,email,passwd,repasswd,acc_type = request.form['name'],request.form['address'],request.form["dob"],int(request.form["cno"]),2000,request.form["email"],request.form["passwd"],request.form["re-passwd"],request.form["acc-type"]# Gathering all info
         for i in range(len(usersDic)):
             if name in usersDic[i]["Name"]:
                 return render_template("signup.html",data="Already a account exist in this name!")
         if passwd == repasswd:# check new passwd == repasswd
-            cur.execute(f"insert into account values('{name}','{accno}','{DOB}','{address}',{cno},{obal},{obal},'{passwd}','{email}');")# change values in DB
+            cur.execute(f"insert into account values('{name}','{accno}','{DOB}','{address}',{cno},{obal},{obal},'{passwd}','{email}','{acc_type}');")# change values in DB
             flash("Registered Successfully!!")
+            updateDic()
+            return render_template("accnoshow.html",name=name,AccNo=accno)
         else:
             flash("Password not matched!")#if not signup
-        updateDic()
-        return render_template("accnoshow.html",name=name,AccNo=accno)
     return render_template("signup.html")
 
-@app.route("/home/<string:accno>/<string:name>",methods=["GET","POST"])# Home page route!
-def home(accno="AXBxxxxxxx",name="xxx"):
-    if status:# only if user logged in with usrname and passwd
-        updatestatus()
+@app.route("/home",methods=["GET","POST"])# Home page route!
+def home():
+    if "Accno" and "username" in session:
+        name = session["username"]
+        accno = session["Accno"]
         return render_template("home.html",name=name,accno=accno)
     else:
-        status_message()
         return redirect(url_for("Login"))
 
 @app.route("/resetpasswd",methods=["GET","POST"])
 def resetPassword():
+    updateDic()
     if request.method=="POST":
         accno,passwd,repasswd,email = request.form["accno"],request.form["passwd"], request.form["re-passwd"], request.form["email"]
         for i in range(len(usersDic)):
             if email in usersDic[i]["Email"]:
-                if status:
-                    if passwd == repasswd:
-                        cur.execute(f"update account set passwd = '{passwd}' where AccNo = '{accno}';")
-                        return redirect(url_for("home",accno=accno,name=usersDic[i]["Name"]))
-                    else:
-                        flash("Password not matched!")
+                if passwd == repasswd:
+                    cur.execute(f"update account set passwd = '{passwd}' where AccNo = '{accno}';")
+                    return redirect(url_for("home",accno=accno,name=usersDic[i]["Name"]))
                 else:
-                    if passwd == repasswd:
-                        cur.execute(f"update account set passwd = '{passwd}' where AccNo = '{accno}';")
-                        return redirect(url_for("Login"))
-                    else:
-                        flash("Password not matched!")
+                    flash("Password not matched!")
             else:
                 flash("Incorrect email Id")
-        updateDic()
+    updateDic()
     return render_template("resetpasswd.html")
 
-@app.route("/changeuserdetails/<string:accno>",methods=["GET","POST"])
-def changeDetails(accno):
-    if status:
+@app.route("/changeuserdetails",methods=["GET","POST"])
+def changeDetails():
+    if "Accno" in session:
+        accno=session["Accno"] 
         if request.method == "POST":
             name,dob,address,contno,email = request.form["name"],request.form["dob"],request.form["address"],request.form["cno"],request.form["email"]
             try:
@@ -163,46 +102,46 @@ def changeDetails(accno):
             except:
                 flash("Error occured! Can't update Details..")
     else:
-        status_message()
         return redirect(url_for("Login"))
     return render_template("changedetails.html",accno=accno)
 
-@app.route("/balanceEnquiry/<string:accno>",methods=["GET","POST"])
-def balanceEq(accno):
-    if status:
+@app.route("/balanceEnquiry",methods=["GET","POST"])
+def balanceEq():
+    if "Accno" in session:
+        accno = session["Accno"]
         cur.execute(f"select * from account where AccNo = '{accno}';")
         res = cur.fetchall()
-        updatestatus()
         return render_template("viewacc.html",datas=res,rep="XXX")
     else:
-        status_message()
         redirect(url_for("Login"))
     return render_template("viewacc.html")
-@app.route("/accountdetails/<string:accno>",methods=["GET","POST"])
-def customerDetails(accno):
-    if status:
+
+@app.route("/accountdetails",methods=["GET","POST"])
+def customerDetails():
+    if "Accno" in session:
+        accno = session["Accno"]
         cur.execute(f"select * from account where AccNo = '{accno}';")
         result = cur.fetchall()
-        updatestatus()
         return render_template("custdetails.html",records=result)
     else:
-        status_message()
         redirect(url_for("Login"))
+    return render_template("custdetails.html")
 
-@app.route("/deleteaccount/<string:accno>",methods=['GET','POST'])
-def deleteAccount(accno):
+@app.route("/deleteaccount",methods=['GET','POST'])
+def deleteAccount():
+    accno = session["Accno"]
     cur.execute("SET SQL_SAFE_UPDATES = 0;")
     cur.execute(f"delete from account where AccNo = '{accno}';")
-    global status
-    status = False
     flash("Successfully Deleted!!!")
     return redirect(url_for('Login')) 
 
-@app.route("/tranfer/<string:accno>",methods=["GET","POST"])
-def transfer(accno):
-    if status:
+@app.route("/transfer",methods=["GET","POST"])
+def transfer():
+    now = datetime.now()
+    if "Accno" in session:
+        accno = session["Accno"]
         if request.method == "POST":
-            fr,amount,to,passwd = request.form["from"],request.form["amount"],request.form["toaccno"],request.form["passwd"]
+            fr,amount,to,passwd,trans_type = request.form["from"],request.form["amount"],request.form["to"],request.form["passwd"],request.form["trans-type"]
             if fr != to and int(amount)!=0 and fr==accno:
                 cur.execute(f"select * from account where AccNo = '{accno}';")
                 resultf = cur.fetchall()
@@ -210,20 +149,50 @@ def transfer(accno):
                     if fdata['passwd']==passwd:
                         for i in range(len(usersDic)):
                             if usersDic[i]["AccNo"] == to:
-                                balancef = fdata['Balance']-int(amount)
-                                cur.execute(f"update account set Balance = {balancef} where AccNo = '{fr}';")
-                                cur.execute(f"select * from account where AccNo = '{to}';")
-                                resultt = cur.fetchall()
-                                for tdata in resultt:
-                                    balancet = tdata['Balance']+int(amount)
-                                    cur.execute(f"update account set Balance = {balancet} where AccNo = '{to}';")
-                                    flash("Transfer Successfull")
-                                    break
+                                if int(amount)>=100:
+                                    if int(amount) < fdata["Balance"]:
+                                        cur.execute(f"select * from account where AccNo = '{to}';")
+                                        result_to = cur.fetchall()
+                                        from_balance = fdata["Balance"]
+                                        for tdata in result_to:
+                                            to_balance = tdata["Balance"]
+                                        if fdata["Balance"] <= 0:
+                                            cur.execute(f"update account set Balance = 0 where AccNo='{fr}'")
+                                        if tdata["Balance"] <= 0:
+                                            cur.execute(f"update account set Balance = 0 where AccNo='{to}'")
+                                        new_from_balance=from_balance-int(amount)
+                                        new_to_balance=to_balance+int(amount)
+
+                                        cur.execute(f"update account set Balance = {new_from_balance} where AccNo = '{fr}'")
+                                        cur.execute(f"update account set Balance = {new_to_balance} where AccNo = '{to}'")
+                                        flash("Transfer Successfull")
+                                        updateDic()
+
+                                        cur.execute(f"select Balance where AccNo = '{fr}'")
+                                        balance_fr=cur.fetchone()
+                                        cur.execute(f"select Balance where AccNo = '{to}'")
+                                        balance_to=cur.fetchone()
+                                        cur.execute(f"insert into trans_details values('{fr}','{trans_type}','{str(now.strftime('%d-%m-%Y %H:%M:%S'))}','{amount}',{balance_fr})")
+                                        cur.execute(f"insert into trans_details values('{to}','{trans_type}','{str(now.strftime('%d-%m-%Y %H:%M:%S'))}','{amount}',{balance_to})")
+
+                                    else:
+                                        flash("Insufficient Balance in your account")
+                                else:
+                                    flash("Transfer amount should be significantly greater than ₹100!")
                     else:
                         flash("Password doesn't matched!")
     else:
-        status_message()
         return redirect(url_for("Login"))
-    return render_template("transfer.html")
+    return render_template("transfer.html",accno=accno)
+
+@app.route("/history",methods=["POST","GET"])
+def trans_history():
+    if "Accno" in session:
+        accno = session["Accno"]
+        name = session["username"]
+        cur.execute(f"select * from trans_details where AccNo='{accno}';")
+        result = cur.fetchall()
+        return render_template("history.html",details=result,name=name)
+    return render_template("history.html")
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- database.py
+++ database.py
@@ -1,7 +1,17 @@
 import mysql.connector as sql
-user_passwd = input("Enter the root password: ")
-mydb = sql.connect(host="localhost",user="root",password=user_passwd,autocommit=True)
+
+usr_name = input("Enter the username:")
+mydb = sql.connect(host="localhost",user="root",password="vishvaa_vsk",autocommit=True)
 cursor = mydb.cursor()
-cursor.execute("create database bankmanagement;")
-cursor.execute("create table account(Name varchar(15),AccNo varchar(15) primary key,DOB varchar(15),Address longtext,ContactNo bigint,OpeningBal int,Balance int,passwd varchar(15),Email longtext);")
-cursor.execute("SET SQL_SAFE_UPDATES = 0;")
+
+try:
+    cursor.execute("create database bankmanagement;")
+except:
+    cursor.execute("use bankmanagement")
+    cursor.execute("SET SQL_SAFE_UPDATES = 0;")
+try:
+    cursor.execute("create table account(Name varchar(30),AccNo varchar(15) primary key,DOB varchar(10),Address longtext,ContactNo bigint,OpeningBal int,Balance int,passwd varchar(15),Email longtext);")
+    cursor.execute("CREATE TABLE trans_details (AccNo VARCHAR(16),FOREIGN KEY (AccNo)REFERENCES account (Accno),trans_type VARCHAR(15),trans_date VARCHAR(30),trans_amount INT,bal_after_trans INT);")
+    print("Database Setup is created successfully!")
+except:
+    print("Database is already set")
